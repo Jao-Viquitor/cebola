b705c1492a6671a3aeb1630e553fa25b07cc994e
Victor Santos <victosantos.ft18@gmail.com>
2021-08-21 13:47:19 -0300

    [Update] Agora funcional, xora Hater

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,41 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Column(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "",
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-         ElevatedButton(style: ElevatedButton.styleFrom(
-           primary: Color.fromRGBO(40, 31, 64, 100),
-           onPrimary: Color.fromRGBO(234, 275, 200, 100)
-         ),
-             onPressed: onPressed, child: Text("Sou examinadora", style: TextStyle(color: Colors.white, fontSize: 20)))
+          ElevatedButton(style: ElevatedButton.styleFrom(
+              primary: Color.fromRGBO(40, 31, 64, 100),
+              onPrimary: Color.fromRGBO(234, 275, 200, 100)
+          ),
+              onPressed: onPressed, child: Text("Sou Examinadora", style: TextStyle(color: Colors.white, fontSize: 20))),
+          ElevatedButton(style: ElevatedButton.styleFrom(
+              primary: Color.fromRGBO(40, 31, 64, 100),
+              onPrimary: Color.fromRGBO(234, 275, 200, 100)
+          ),
+              onPressed: onPressed, child: Text("Sou Instrutora", style: TextStyle(color: Colors.white, fontSize: 20))),
+          ElevatedButton(style: ElevatedButton.styleFrom(
+              primary: Color.fromRGBO(40, 31, 64, 100),
+              onPrimary: Color.fromRGBO(234, 275, 200, 100)
+          ),
+              onPressed: onPressed, child: Text("Sou Organista", style: TextStyle(color: Colors.white, fontSize: 20)))
         ],
+
+      )
+      )
     );
+  }
+
 
   void onPressed() {
   }
 }
+
+
